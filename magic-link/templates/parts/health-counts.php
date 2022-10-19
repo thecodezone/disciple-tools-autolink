02af6101fc7045f3b8b17b87512d8cc5bc04d05e
Craig Wann <craig@codezone.io>
2022-10-19 13:11:26 -0500

    Positioning church health counts.

--- magic-link/templates/parts/health-counts.php
+++ magic-link/templates/parts/health-counts.php
@@ -1,12 +1,11 @@
 <?php
 $group = DT_Posts::get_post( 'groups', $church['ID'] );
 ?>
-<div class="church-counts">
+<div class="church__counts">
     <?php foreach ( $church_count_fields as $key => $field ) : ?>
-        <div class="church-count">
-            <span class="church-count__value"><?php echo $group[$key] ?? 0; ?></span>
-            <span class="church-count__label"><?php echo $field['name']; ?></span>
-            <img class="church-count__icon" src="<?php echo $field['icon']; ?>" alt="<?php echo $field['name']; ?>" width="25" height="25">
+        <div class="church__count">
+            <span class="count__value"><?php echo esc_html( $group[$key] ?? 0 ); ?></span>
+            <img class="count__icon" src="<?php echo esc_html( $field['icon'] ); ?>" alt="<?php echo esc_attr( $field['name'] ); ?>" width="25" height="25">
         </div>
     <?php endforeach; ?>
 </div>
