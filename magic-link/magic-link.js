ded750313d7437762c02727c02650bbeae1cd29f
Craig Wann <craig@codezone.io>
2022-10-24 13:40:44 -0500

    Deleting custom icon components in favor of dt-icon.

--- magic-link/magic-link.js
+++ magic-link/magic-link.js
@@ -5,17 +5,10 @@ loaded(() => {
 })
 
 import "dt-web-components";
-import "./js/hamburger.js";
 import "./js/menu.js";
 import "./js/button.js";
-import "./js/chevronRight";
-import "./js/chevronDown";
-import "./js/chevronUp";
-import "./js/copy";
 import "./js/copyText";
-import "./js/check";
 import "./js/churchHealthCircle";
 import "./js/collapse";
 import "./js/church";
-import "./js/link";
 import "./js/icon";
