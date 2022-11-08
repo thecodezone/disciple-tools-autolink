b6c863048fa3b40d4f6137efad30c3d96bf81e71
Micah Mills <micahmills@gmail.com>
2022-11-08 10:07:01 -0500

    Added link to footer

--- magic-link/templates/parts/footer.php
+++ magic-link/templates/parts/footer.php
@@ -1,5 +1,5 @@
 </div> <!-- .app__inner -->
     <footer class="footer">
-        <p><?php esc_html_e( 'Powered by', 'disciple-tools-autolink' ) ?>  <a href="">Disciple.Tools</a></p>
+        <p><?php esc_html_e( 'Powered by', 'disciple-tools-autolink' ) ?>  <a href="https://disciple.tools/">Disciple.Tools</a></p>
     </footer>
 </div> <!-- .app__wrapper -->
