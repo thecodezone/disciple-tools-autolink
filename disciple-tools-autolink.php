36727c5070a071d28c4292d78804038455b08657
Micah Mills <micahmills@gmail.com>
2022-11-29 00:51:51 -0500

    Now using dt-components repo instead of private one. also version bump

--- disciple-tools-autolink.php
+++ disciple-tools-autolink.php
@@ -5,7 +5,7 @@
  * Description: Disciple.Tools - Autolink is intended to help developers and integrator jumpstart their extension of the Disciple.Tools system.
  * Text Domain: disciple-tools-autolink
  * Domain Path: /languages
- * Version:  0.4.5
+ * Version:  0.4.6
  * Author URI: https://github.com/DiscipleTools
  * GitHub Plugin URI: https://github.com/DiscipleTools/disciple-tools-autolink
  * Requires at least: 4.7.0
