7c15bd450ee8a61eba40c51593abb478a4a04dcb
Craig Wann <craig@codezone.io>
2023-05-04 03:55:49 -0500

    Checking for invalid child nesting in tree.

--- magic-link/controllers/tree.php
+++ magic-link/controllers/tree.php
@@ -1,46 +1,46 @@
 <?php
 
-class Disciple_Tools_Autolink_Tree_Controller extends Disciple_Tools_Autolink_Controller
-{
+class Disciple_Tools_Autolink_Tree_Controller extends Disciple_Tools_Autolink_Controller {
     const nonce = 'dt_autolink_tree';
     private $tree_chart = null;
 
-    public function __construct()
-    {
-        $this->functions = Disciple_Tools_Autolink_Magic_Functions::instance();
+    public function __construct() {
+        $this->functions  = Disciple_Tools_Autolink_Magic_Functions::instance();
         $this->tree_chart = Disciple_Tools_Autolink_Groups_Tree::instance();
     }
 
-    public function show($params = [])
-    {
+    public function show( $params = [] ) {
         $magic_link = Disciple_Tools_Autolink_Magic_User_App::instance();
-        $data = $this->global_data();
-        extract($data);
-        $action = 'tree';
+        $data       = $this->global_data();
+        extract( $data );
+        $action    = 'tree';
         $fetch_url = '/wp-json/autolink/v1/' . $magic_link->parts['type'];
-        $parts = $magic_link->parts;
+        $parts     = $magic_link->parts;
 
-        include(__DIR__ . '/../templates/tree.php');
+        include( __DIR__ . '/../templates/tree.php' );
     }
 
-    public function process(WP_REST_Request $request, $params, $user_id)
-    {
-        if (!isset($params['data']['previous_parent'])) {
+    public function process( WP_REST_Request $request, $params, $user_id ) {
+        if ( ! isset( $params['data']['previous_parent'] ) ) {
             $params['data']['previous_parent'] = 'domenu-0';
         }
-        if ((! isset($params['data']['new_parent']) || (! isset($params['data']['self'])))) {
+        if ( ( ! isset( $params['data']['new_parent'] ) || ( ! isset( $params['data']['self'] ) ) ) ) {
             return 'false';
         }
 
-        $group = DT_Posts::get_post('groups', $params['data']['self'], true, false);
+        $group     = DT_Posts::get_post( 'groups', $params['data']['self'], true, false );
+        $new_group = ( $params['data']['new_parent'] != 'domenu-0' ) ? DT_Posts::get_post( 'groups', $params['data']['new_parent'], true, false ) : null;
 
-        if ($params['data']['new_parent'] == 'domenu-0'
-            && ((int) $group["assigned_to"]["id"] !== $user_id)) {
+        if ( ! $new_group
+             && ( (int) $group["assigned_to"]["id"] !== $user_id ) ) {
+            return "reload";
+        } elseif ( $new_group
+                   && ( (int) $new_group['assigned_to']["id"] !== $user_id ) ) {
             return "reload";
         }
         global $wpdb;
 
-        $wpdb->query($wpdb->prepare(
+        $wpdb->query( $wpdb->prepare(
             "DELETE
                 FROM $wpdb->p2p
                 WHERE p2p_from = %s
@@ -48,25 +48,24 @@ class Disciple_Tools_Autolink_Tree_Controller extends Disciple_Tools_Autolink_Co
                     AND p2p_type = 'groups_to_groups'",
             $params['data']['self'],
             $params['data']['previous_parent']
-        ));
+        ) );
 
-        if ($params['data']['new_parent'] !== 'domenu-0') {
-            $wpdb->query($wpdb->prepare(
+        if ( $params['data']['new_parent'] !== 'domenu-0' ) {
+            $wpdb->query( $wpdb->prepare(
                 "INSERT INTO $wpdb->p2p (p2p_from, p2p_to, p2p_type)
                     VALUES (%s, %s, 'groups_to_groups');
             ",
                 $params['data']['self'],
                 $params['data']['new_parent']
-            ));
+            ) );
 
-            do_action('p2p_created_connection', $wpdb->insert_id);
+            do_action( 'p2p_created_connection', $wpdb->insert_id );
         }
 
         return true;
     }
 
-    public function data(WP_REST_Request $request, $params, $user_id)
-    {
-        echo wp_json_encode($this->tree_chart->tree());
+    public function data( WP_REST_Request $request, $params, $user_id ) {
+        echo wp_json_encode( $this->tree_chart->tree() );
     }
 }
--- magic-link/templates/tree.php
+++ magic-link/templates/tree.php
@@ -1,12 +1,12 @@
 <style>
     .dd3-content {
-       background: -webkit-linear-gradient(top, #fdfdfd 10%, #c1efc3 100%);
-       background: linear-gradient(top, #fdfdfd 10%, #c1efc3 100%);
+        background: -webkit-linear-gradient(top, #fdfdfd 10%, #c1efc3 100%);
+        background: linear-gradient(top, #fdfdfd 10%, #c1efc3 100%);
     }
 
-    .dd-item--owned  > .dd3-content {
-       background: -webkit-linear-gradient(top, #f4f4f4 10%, #c9c9c900 25%);
-       background: linear-gradient(top, #f4f4f4 10%, #c9c9c900 25%);
+    .dd-item--owned > .dd3-content {
+        background: -webkit-linear-gradient(top, #f4f4f4 10%, #c9c9c900 25%);
+        background: linear-gradient(top, #f4f4f4 10%, #c9c9c900 25%);
     }
 
     #u .dd3-content {
@@ -14,7 +14,7 @@
         background: -webkit-linear-gradient(top, #f4f4f4 10%, #fbf2c4 100%);
     }
 
-    #u >.dd-handle {
+    #u > .dd-handle {
         display: none;
         pointer-events: none;
     }
@@ -59,27 +59,29 @@
 
     <dt-tile title="<?php esc_html_e( 'Church Tree' ); ?>">
         <div class="section__inner">
-                <div class="loading-spinner__wrapper">
-                    <div class="loading-spinner active"></div>
-                </div>
-                <dt-alert class="alert--error" context="alert">
-                    <?php esc_html_e( 'An unexpected error has occurred.', 'disciple-tools-autolink' ); ?>
-                </dt-alert>
-                <dt-alert class="alert--root-child" context="alert">
-                    <?php esc_html_e( 'First generation groups must be assigned to you.', 'disciple-tools-autolink' ); ?>
-                </dt-alert>
-                <div id="wrapper"></div>
+            <div class="loading-spinner__wrapper">
+                <div class="loading-spinner active"></div>
             </div>
+            <dt-alert class="alert--error"
+                      context="alert">
+                <?php esc_html_e( 'An unexpected error has occurred.', 'disciple-tools-autolink' ); ?>
+            </dt-alert>
+            <dt-alert class="alert--invalid"
+                      context="alert">
+                <?php esc_html_e( 'Invalid group nesting.', 'disciple-tools-autolink' ); ?>
+            </dt-alert>
+            <div id="wrapper"></div>
         </div>
-    </dt-tile>
+</div>
+</dt-tile>
 
 </div>
 
 <?php include( 'parts/app-footer.php' ); ?>
 
 <script>
-window.load_tree = () => {
-  jQuery('#wrapper').html(`
+    window.load_tree = () => {
+        jQuery('#wrapper').html(`
 <div class="dd" id="domenu-0">
   <button class="dd-new-item" style="display: none;">+</button>
   <li class="dd-item-blueprint">
@@ -98,160 +100,164 @@ window.load_tree = () => {
   <ol class="dd-list"></ol>
 </div>
   `)
-  window.post_item( 'tree', {} )
-    .done(function(data){
-        jQuery( '.loading-spinner' ).addClass( 'active' )
-        window.load_domenu(data)
-        jQuery( '.loading-spinner' ).removeClass( 'active' )
-    })
-}
-
-window.load_domenu = ( data ) => {
-
-const find_item = ( id, tree = null ) => {
-    if ( !tree ) {
-        tree = data.tree
+        window.post_item('tree', {})
+            .done(function (data) {
+                jQuery('.loading-spinner').addClass('active')
+                window.load_domenu(data)
+                jQuery('.loading-spinner').removeClass('active')
+            })
     }
 
-    for ( let i = 0; i < tree.length; i++ ) {
+    window.load_domenu = (data) => {
 
-        if ( tree[i].id == id ) {
-            return tree[i]
-        }
-
-        if ( tree[i].children ) {
-            const item = find_item( id, tree[i].children )
-            if ( item ) {
-                return item
+        const find_item = (id, tree = null) => {
+            if (!tree) {
+                tree = data.tree
             }
-        }
-    }
-    return null
-}
 
-const domenu = jQuery( '#domenu-0' ).domenu({
-    data: JSON.stringify( data.tree ),
-    maxDepth: 500,
-    refuseConfirmDelay: 500, // does not delete immediately but requires a second click to confirm.
-    select2:                {
-      support:     false, // Enable Select2 support
-    }
-}).parseJson()
-    .onItemDrop(function( e ) {
-        if ( typeof e.prevObject !== 'undefined' && typeof e[0].id !== 'undefined' ) { // runs twice on drop. with and without prevObject
-            jQuery( '.loading-spinner' ).addClass( 'active' )
+            for (let i = 0; i < tree.length; i++) {
 
-            let new_parent = e[0].parentNode.parentNode.id
-            let self = e[0].id
+                if (tree[i].id == id) {
+                    return tree[i]
+                }
 
-            setListData(data)
-
-            let prev_parent_object = jQuery( '#' +e[0].id )
-            let previous_parent = prev_parent_object.data( 'prev_parent' )
-
-            prev_parent_object.attr( 'data-prev_parent', new_parent ) // set previous
-
-
-            if ( new_parent !== previous_parent ) {
-                jQuery('dt-alert').each(function() {
-                    jQuery(this).removeClass('active')
-                });
-                window.post_item( 'onItemDrop', { new_parent: new_parent, self, previous_parent: previous_parent } ).done(function( result ){
-                    if (result === 'reload') {
-                        jQuery( '.alert--root-child' ).addClass( 'active' )
-                        load_tree()
-                    } else if (!result) {
-                        jQuery( '.alert--error' ).addClass( 'active' )
+                if (tree[i].children) {
+                    const item = find_item(id, tree[i].children)
+                    if (item) {
+                        return item
                     }
-                })
-            }
-        }
-    })
-    .onItemSetParent(function( e ) {
-        if ( typeof e[0] !== 'undefined' ) {
-            jQuery( '#' + e[0].id + ' button.item-remove:first' ).hide();
-        }
-    })
-    .onItemUnsetParent(function( e ) {
-        if ( typeof e[0] !== 'undefined' ) {
-            jQuery( '#' + e[0].id + ' button.item-remove:first' ).show();
-        }
-    })
-
-
-    // list prep
-    jQuery.each( jQuery( '#domenu-0 .item-name' ), function( i,v ){
-        // move and set the title to id
-        let id = jQuery( this ).html()
-        if (id !== 'u') {
-            id = parseInt(id)
-        }
-        const item = find_item( id )
-
-        jQuery( this ).parent().parent().attr( 'id', id )
-
-        if ( ! item ){
-            return;
-        }
-        if ( item['coaching'] ) {
-            jQuery( this ).parent().parent().addClass( 'dd-item--coaching' )
-        } else {
-            jQuery( this ).parent().parent().addClass( 'dd-item--owned' )
-        }
-        if ( item && item['has_parent'] ) {
-            jQuery( this ).parent().parent().addClass( 'dd-item--has-parent' )
-        }
-    })
-
-    // set the previous parent data element
-    function setListData(data) {
-        jQuery.each(data.parent_list, function (ii, vv) {
-            if (vv !== null && vv !== "undefined") {
-                let target = jQuery('#' + ii)
-                if (target.length > 0) {
-                    target.attr('data-prev_parent', vv)
                 }
             }
+            return null
+        }
+
+        const domenu = jQuery('#domenu-0').domenu({
+            data: JSON.stringify(data.tree),
+            maxDepth: 500,
+            refuseConfirmDelay: 500, // does not delete immediately but requires a second click to confirm.
+            select2: {
+                support: false, // Enable Select2 support
+            }
+        }).parseJson()
+            .onItemDrop(function (e) {
+                if (typeof e.prevObject !== 'undefined' && typeof e[0].id !== 'undefined') { // runs twice on drop. with and without prevObject
+                    jQuery('.loading-spinner').addClass('active')
+
+                    let new_parent = e[0].parentNode.parentNode.id
+                    let self = e[0].id
+
+                    setListData(data)
+
+                    let prev_parent_object = jQuery('#' + e[0].id)
+                    let previous_parent = prev_parent_object.data('prev_parent')
+
+                    prev_parent_object.attr('data-prev_parent', new_parent) // set previous
+
+
+                    if (new_parent !== previous_parent) {
+                        jQuery('dt-alert').each(function () {
+                            jQuery(this).removeClass('active')
+                        });
+                        window.post_item('onItemDrop', {
+                            new_parent: new_parent,
+                            self,
+                            previous_parent: previous_parent
+                        }).done(function (result) {
+                            if (result === 'reload') {
+                                jQuery('.alert--invalid').addClass('active')
+                                load_tree()
+                            } else if (!result) {
+                                jQuery('.alert--error').addClass('active')
+                            }
+                        })
+                    }
+                }
+            })
+            .onItemSetParent(function (e) {
+                if (typeof e[0] !== 'undefined') {
+                    jQuery('#' + e[0].id + ' button.item-remove:first').hide();
+                }
+            })
+            .onItemUnsetParent(function (e) {
+                if (typeof e[0] !== 'undefined') {
+                    jQuery('#' + e[0].id + ' button.item-remove:first').show();
+                }
+            })
+
+
+        // list prep
+        jQuery.each(jQuery('#domenu-0 .item-name'), function (i, v) {
+            // move and set the title to id
+            let id = jQuery(this).html()
+            if (id !== 'u') {
+                id = parseInt(id)
+            }
+            const item = find_item(id)
+
+            jQuery(this).parent().parent().attr('id', id)
+
+            if (!item) {
+                return;
+            }
+            if (item['coaching']) {
+                jQuery(this).parent().parent().addClass('dd-item--coaching')
+            } else {
+                jQuery(this).parent().parent().addClass('dd-item--owned')
+            }
+            if (item && item['has_parent']) {
+                jQuery(this).parent().parent().addClass('dd-item--has-parent')
+            }
         })
+
+        // set the previous parent data element
+        function setListData(data) {
+            jQuery.each(data.parent_list, function (ii, vv) {
+                if (vv !== null && vv !== "undefined") {
+                    let target = jQuery('#' + ii)
+                    if (target.length > 0) {
+                        target.attr('data-prev_parent', vv)
+                    }
+                }
+            })
+        }
+
+        setListData(data)
+
+        // show delete for last item
+        jQuery("li:not(:has(>ol)) .item-remove").show()
+        // set properties
+        jQuery.each(jQuery('.item-name'), function (ix, vx) {
+            let old_title = jQuery(this).html()
+            jQuery(this).html(data.title_list[old_title])
+        })
+
+        window.setTimeout(function () {
+            domenu.collapse(el => jQuery(el).hasClass('dd-item--coaching'))
+        }, 1)
+
     }
 
-    setListData(data)
-
-    // show delete for last item
-    jQuery( "li:not(:has(>ol)) .item-remove" ).show()
-    // set properties
-    jQuery.each(jQuery( '.item-name' ), function( ix,vx ) {
-        let old_title = jQuery( this ).html()
-        jQuery( this ).html( data.title_list[old_title] )
-    })
-
-    window.setTimeout(function(){
-        domenu.collapse(el => jQuery(el).hasClass('dd-item--coaching'))
-    }, 1)
-
-}
-
-window.post_item = ( action, data ) => {
-  jQuery('.loading-spinner').addClass('active')
-  return jQuery.ajax({
-    type: "POST",
-    data: JSON.stringify({ action: action, parts: <?php echo wp_json_encode( $parts ); ?>, data: data }),
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    url: `<?php echo esc_url( $fetch_url ); ?>`,
-    beforeSend: function (xhr) {
-      xhr.setRequestHeader('X-WP-Nonce', `<?php echo wp_create_nonce( 'wp_rest' ); ?>` )
+    window.post_item = (action, data) => {
+        jQuery('.loading-spinner').addClass('active')
+        return jQuery.ajax({
+            type: "POST",
+            data: JSON.stringify({action: action, parts: <?php echo wp_json_encode( $parts ); ?>, data: data}),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            url: `<?php echo esc_url( $fetch_url ); ?>`,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('X-WP-Nonce', `<?php echo wp_create_nonce( 'wp_rest' ); ?>`)
+            }
+        })
+            .done(function (e) {
+                jQuery('#error').html(e)
+                jQuery('.loading-spinner').removeClass('active')
+            })
+            .fail(function (e) {
+                jQuery('#error').html(e)
+                jQuery('.loading-spinner').removeClass('active')
+            })
     }
-  })
-    .done(function(e) {
-      jQuery('#error').html(e)
-      jQuery('.loading-spinner').removeClass('active')
-    })
-    .fail(function(e) {
-      jQuery('#error').html(e)
-      jQuery('.loading-spinner').removeClass('active')
-    })
-}
 
-load_tree()
+    load_tree()
 </script>
