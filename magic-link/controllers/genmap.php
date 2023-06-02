2892976ebd085bc914c79b66b3f7245d4927f768
Craig Wann <craig@codezone.io>
2023-06-02 12:38:55 -0500

    Scoping some print styles to genmap.
    Signed-off-by: Craig Wann <craig@codezone.io>

--- magic-link/controllers/genmap.php
+++ magic-link/controllers/genmap.php
@@ -2,13 +2,14 @@
 
 class Disciple_Tools_Autolink_Genmap_Controller extends Disciple_Tools_Autolink_Controller {
     public function show( $params = [] ) {
-        if ( !class_exists( 'DT_Genmapper_Groups_chart' ) ) {
+        if ( ! class_exists( 'DT_Genmapper_Groups_chart' ) ) {
             wp_redirect( $this->functions->get_app_link() );
         }
 
         $data = $this->global_data();
         extract( $data );
-        $action = 'genmap';
+        $action    = 'genmap';
+        $app_class = 'app--genmap';
 
         include( __DIR__ . '/../templates/genmap.php' );
     }
