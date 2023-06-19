4d71dae199c27473498e66169242f2f583c5a3d4
Craig Wann <craig@codezone.io>
2023-06-19 10:04:48 -0500

    Fixing delete nonce.

--- magic-link/controllers/group.php
+++ magic-link/controllers/group.php
@@ -134,10 +134,9 @@ class Disciple_Tools_Autolink_Group_Controller extends Disciple_Tools_Autolink_C
      */
     public function delete( $params = [] ) {
         $app_controller = new Disciple_Tools_Autolink_App_Controller();
-
-        $nonce        = sanitize_key( wp_unslash( $_GET['_wpnonce'] ?? '' ) );
-        $verify_nonce = $nonce && wp_verify_nonce( $nonce, self::NONCE );
-        $group_id     = sanitize_text_field( wp_unslash( $_GET['post'] ?? '' ) );
+        $nonce          = sanitize_key( wp_unslash( $_GET['_wpnonce'] ?? '' ) );
+        $verify_nonce   = $nonce && wp_verify_nonce( $nonce, 'dt_autolink_delete_group' );
+        $group_id       = sanitize_text_field( wp_unslash( $_GET['post'] ?? '' ) );
 
         if ( ! $verify_nonce ) {
             $app_controller->show( [ 'error' => __( 'Unauthorized action. Please refresh the page and try again.', 'disciple-tools-autolink' ) ] );
