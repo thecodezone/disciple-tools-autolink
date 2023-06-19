44cbfd9c98931c850048e9a0206d68876ad01442
Craig Wann <craig@codezone.io>
2023-06-19 10:42:43 -0500

    Persisting register and login field values on error.

--- magic-link/templates/login.php
+++ magic-link/templates/login.php
@@ -13,45 +13,45 @@
         <div class="section__inner">
             <div class="logo">
                 <img src="<?php echo esc_url( $logo_url ) ?>"
-                    alt="Disciple.Tools"
-                    class="logo__image">
+                     alt="Disciple.Tools"
+                     class="logo__image">
             </div>
 
             <form action="<?php echo esc_attr( $form_action ) ?>"
-                method="POST">
+                  method="POST">
                 <?php wp_nonce_field( 'dt_autolink_login' ); ?>
 
-                <?php if ( !empty( $error ) ) : ?>
+                <?php if ( ! empty( $error ) ) : ?>
                     <dt-alert context="alert"
-                            dismissable>
+                              dismissable>
                         <?php echo esc_html( strip_tags( $error ) ) ?>
                     </dt-alert>
                 <?php endif; ?>
 
                 <dt-text name="username"
-                        placeholder="<?php esc_attr_e( 'Username or Email Address', 'disciple-tools-autolink' ); ?>"
-                        value="<?php echo esc_attr( $username ); ?>"
-                        required
-                        tabindex="1"
-                        ></dt-text>
+                         placeholder="<?php esc_attr_e( 'Username or Email Address', 'disciple-tools-autolink' ); ?>"
+                         value="<?php echo esc_attr( $username ); ?>"
+                         required
+                         tabindex="1"
+                ></dt-text>
                 <dt-text name="password"
-                        placeholder="<?php esc_attr_e( 'Password', 'disciple-tools-autolink' ); ?>"
-                        value=""
-                        type="password"
-                        tabindex="2"
-                        required></dt-text>
+                         placeholder="<?php esc_attr_e( 'Password', 'disciple-tools-autolink' ); ?>"
+                         value="<?php echo esc_attr( $password ); ?>"
+                         type="password"
+                         tabindex="2"
+                         required></dt-text>
 
                 <div class="login__buttons">
                     <dt-button context="success"
-                            tabindex="3"
-                            type="submit">
+                               tabindex="3"
+                               type="submit">
                         <?php esc_html_e( 'Login', 'disciple-tools-autolink' ) ?>
                     </dt-button>
 
                     <dt-button context="link"
-                            href="<?php echo esc_url( $register_url ); ?>"
-                            tabindex="`4"
-                            title="<?php esc_attr_e( 'Create Account', 'disciple-tools-autolink' ); ?>">
+                               href="<?php echo esc_url( $register_url ); ?>"
+                               tabindex="`4"
+                               title="<?php esc_attr_e( 'Create Account', 'disciple-tools-autolink' ); ?>">
                         <?php esc_html_e( 'Create Account', 'disciple-tools-autolink' ) ?>
                         <dt-chevron-right></dt-chevron-right>
                     </dt-button>
@@ -60,9 +60,10 @@
         </div>
     </dt-tile>
     <div class="login__footer">
-        <dt-button context="link" href="<?php echo esc_url( $reset_url ); ?>">
+        <dt-button context="link"
+                   href="<?php echo esc_url( $reset_url ); ?>">
             <?php esc_html_e( 'Forgot Password?', 'disciple-tools-autolink' ); ?>
-            </dt-button>
-</div>
+        </dt-button>
+    </div>
 
-<?php include( 'parts/footer.php' ); ?>
+    <?php include( 'parts/footer.php' ); ?>
