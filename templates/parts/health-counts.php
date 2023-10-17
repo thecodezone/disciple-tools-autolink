257666f958d0389a8fa0ab2d1a77bda5b104a307
Craig Wann <craig@codezone.io>
2023-10-17 17:21:20 -0500

    Adding group pagination.

--- templates/parts/health-counts.php
+++ templates/parts/health-counts.php
@@ -2,9 +2,9 @@
 $group = DT_Posts::get_post( 'groups', $church['ID'], false, false );
 ?>
 <div class="church__counts">
-    <?php foreach ( $church_count_fields as $key => $field ) : ?>
+	<?php foreach ( $church_count_fields as $key => $field ) : ?>
         <div class="church__count"
-             data-churchid="<?php echo esc_attr( $church['ID'] ); ?>"
+             data-churchId="<?php echo esc_attr( $church['ID'] ); ?>"
              data-field="<?php echo esc_attr( $key ) ?>">
             <dt-modal context="default"
                       hideHeader>
@@ -18,21 +18,21 @@ $group = DT_Posts::get_post( 'groups', $church['ID'], false, false );
                 </div>
                 <span slot="content">
                 <app-church-health-field
-                    id="<?php echo esc_attr( 'groups_' . $church['ID'] . '_' . $key ); ?>"
-                    name="<?php echo esc_attr( $key ); ?>"
-                    icon="<?php echo esc_html( $field['icon'] ); ?>"
-                    label="<?php echo esc_attr( $field['name'] ); ?>"
-                    onchange=""
-                    value="<?php echo esc_html( $group[ $key ] ?? 0 ); ?>"
-                    postType="groups"
-                    postID="<?php echo esc_attr( $church['ID'] ) ?>"
-                    apiRoot="<?php echo esc_attr( "/wp-json/" ) ?>"
-                    min="0"
-                    placeholder="0"
-                    nonce=<?php echo esc_attr( wp_create_nonce( 'wp_rest' ) ); ?>
+                        id="<?php echo esc_attr( 'groups_' . $church['ID'] . '_' . $key ); ?>"
+                        name="<?php echo esc_attr( $key ); ?>"
+                        icon="<?php echo esc_html( $field['icon'] ); ?>"
+                        label="<?php echo esc_attr( $field['name'] ); ?>"
+                        onchange=""
+                        value="<?php echo esc_html( $group[ $key ] ?? 0 ); ?>"
+                        postType="groups"
+                        postID="<?php echo esc_attr( $church['ID'] ) ?>"
+                        apiRoot="<?php echo esc_attr( "/wp-json/" ) ?>"
+                        min="0"
+                        placeholder="0"
+                        nonce=<?php echo esc_attr( wp_create_nonce( 'wp_rest' ) ); ?>
                 ></app-church-health-field>
             </span>
             </dt-modal>
         </div>
-    <?php endforeach; ?>
+	<?php endforeach; ?>
 </div>
