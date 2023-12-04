8d83d4f11dd01a10dae049effcbc619cce518ad1
Craig Wann <craig@codezone.io>
2023-12-04 13:44:25 -0600

    Making rename script executable.

--- .rename.sh
+++
@@ -1,7 +0,0 @@
-find ./ -type f -print0 | xargs -0 perl -pi -e 's/DT_Plugin/DT_Plugin/g';
-find ./ -type f -print0 | xargs -0 perl -pi -e 's/dt_plugin/dt_plugin/g';
-find ./ -type f -print0 | xargs -0 perl -pi -e 's/dt-plugin/dt-plugin/g';
-find ./ -type f -print0 | xargs -0 perl -pi -e 's/starter_post_type/starter_post_type/g';
-find ./ -type f -print0 | xargs -0 perl -pi -e 's/DT Plugin/DT Plugin/g';
-mv dt-plugin.php dt-plugin.php
-rm .rename.sh
