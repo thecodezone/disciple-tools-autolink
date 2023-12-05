47d5f3ea591165b12a0300cad8063a809c40dae1
Craig Wann <craig@codezone.io>
2023-12-04 21:11:59 -0600

    Commenting out demo components

--- src/Providers/MagicLinkServiceProvider.php
+++ src/Providers/MagicLinkServiceProvider.php
@@ -2,16 +2,14 @@
 
 namespace DT\Plugin\Providers;
 
-use DT\Plugin\MagicLinks\UserMagicLink;
-
 class MagicLinkServiceProvider extends ServiceProvider {
 	public function register(): void {
 	}
 
 	public function boot(): void {
-		$this->container->singleton(
-			UserMagicLink::class
-		);
-		$this->container->make( UserMagicLink::class );
+		//  $this->container->singleton(
+		//      UserMagicLink::class
+		//  );
+		//  $this->container->make( UserMagicLink::class );
 	}
 }
