3b51cbc805c910dddc5534d8040ab1bad55a15ca
Craig Wann <craig@codezone.io>
2023-12-10 12:31:23 -0600

    Excluding directories from rename.

--- .rename.sh
+++ .rename.sh
@@ -7,12 +7,17 @@ SNAKE_CASE="dt_plugin"
 UPPER_CAMEL_CASE="DT_Plugin"
 KEBAB_CASE="dt-plugin"
 
-find ./ -type f -print0 | xargs -0 perl -pi -e "s/DT Plugin/$NAME/g";
-find ./ -type f -print0 | xargs -0 perl -pi -e "s/dt\/plugin/$PACKAGE/g";
-find ./ -type f -print0 | xargs -0 perl -pi -e "s/DT\\Plugin/$NAMESPACE/g";
-find ./ -type f -print0 | xargs -0 perl -pi -e "s/DT\\\\Plugin/$NAMESPACE_ESCAPED/g";
-find ./ -type f -print0 | xargs -0 perl -pi -e "s/DT_Plugin/$UPPER_CAMEL_CASE/g";
-find ./ -type f -print0 | xargs -0 perl -pi -e "s/dt_plugin/$SNAKE_CASE/g";
-find ./ -type f -print0 | xargs -0 perl -pi -e "s/dt-plugin/$KEBAB_CASE/g";
+# Exclude specified directories
+EXCLUDE_DIRS="-path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./.idea"
+
+# Replace strings in files excluding specified directories
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT Plugin/$NAME/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt\/plugin/$PACKAGE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT\\Plugin/$NAMESPACE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT\\\\Plugin/$NAMESPACE_ESCAPED/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT_Plugin/$UPPER_CAMEL_CASE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt_plugin/$SNAKE_CASE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt-plugin/$KEBAB_CASE/g"
+
 mv dt-plugin.php $FILENAME
 rm .rename.sh
