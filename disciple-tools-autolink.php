b83012be0060b9c22ea76c51103f0d9cabd134cd
Craig Wann <craig@codezone.io>
2024-02-20 10:59:05 -0600

    Versioning.

--- disciple-tools-autolink.php
+++ disciple-tools-autolink.php
@@ -6,7 +6,7 @@
  * Description: Provides a simplified interface for managing and visualizing your Disciple.Tools network. Share access with your downstream leaders to easily build your group tree.
  * Text Domain: disciple-tools-autolink
  * Domain Path: /languages
- * Version:  1.0.10
+ * Version:  1.0.11
  * Author URI: https://www.eastwest.org/
  * GitHub Plugin URI: https://github.com/thecodezone/disciple-tools-autolink
  * Requires at least 4.7.0
