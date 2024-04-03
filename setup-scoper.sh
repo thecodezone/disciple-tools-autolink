3a0280c901654185259a6902f04f1a3d7f82ca97
Craig Wann <craig@codezone.io>
2024-04-03 15:34:16 -0500

    Making sure we only get 8.1 deps.

--- setup-scoper.sh
+++ setup-scoper.sh
@@ -3,5 +3,5 @@ source $HOME/.bashrc
 if ! command -v php-scoper > /dev/null 2>&1; then
   echo "PHP Scoper is not installed. Installing..."
   composer global config --no-plugins allow-plugins.wpify/scoper true
-  composer global require wpify/scoper
+  composer global require wpify/scoper --ignore-platform-reqs
 fi
