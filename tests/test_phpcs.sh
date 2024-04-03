6dd89469b6a364f487d5777e5571d06fe3af308e
Craig Wann <craig@codezone.io>
2024-04-03 11:58:53 -0500

    Using local phpcs.

--- tests/test_phpcs.sh
+++ tests/test_phpcs.sh
@@ -4,9 +4,4 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-if [ "$(php -r 'echo version_compare( phpversion(), "8.0", ">=" ) ? 1 : 0;')" != 1 ] ; then
-    vendor/bin/phpcs disciple-tools-autolink.php
-    exit
-fi
-
-eval vendor/bin/phpcs
+vendor/bin/phpcs disciple-tools-autolink.php
