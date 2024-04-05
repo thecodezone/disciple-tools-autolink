ba03725fa6fe76893385746499407dbad210c999
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-04-05 18:58:50 +0530

    swap genmap mygroups
    commite to genmap mygroups buttons

--- resources/views/partials/tools-menu.php
+++ resources/views/partials/tools-menu.php
@@ -6,18 +6,18 @@ $action = $action ?? null;
 ?>
 
 <div class="container">
-	<dt-button context="<?php echo !$action ? 'primary' : 'inactive'; ?>"
-	           href="<?php echo esc_url( route_url() ); ?>">
-		<?php echo esc_html( __( "My ", 'disciple-tools-autolink' ) . groups_label() ); ?>
-	</dt-button>
+    <?php if ( function_exists( 'dt_genmapper_metrics' ) ): ?>
+        <dt-button context="<?php echo $action === 'genmap' ? 'primary' : 'inactive'; ?>"
+                   href="<?php echo esc_url( route_url() ); ?>">
+            <?php echo esc_html( __( 'GenMap', 'disciple-tools-autolink' ) ); ?>
+        </dt-button>
+    <?php endif; ?>
 	<dt-button context="<?php echo $action === 'coaching-tree' ? 'primary' : 'inactive'; ?>"
 	           href="<?php echo esc_url( route_url( 'coaching-tree' ) ) ?>">
 		<?php echo esc_html( __( 'Coaching Tree', 'disciple-tools-autolink' ) ); ?>
 	</dt-button>
-	<?php if ( function_exists( 'dt_genmapper_metrics' ) ): ?>
-		<dt-button context="<?php echo $action === 'genmap' ? 'primary' : 'inactive'; ?>"
-		           href="<?php echo esc_url( route_url( 'genmap' ) ); ?>">
-			<?php echo esc_html( __( 'GenMap', 'disciple-tools-autolink' ) ); ?>
-		</dt-button>
-	<?php endif; ?>
+    <dt-button context="<?php echo !$action ? 'primary' : 'inactive'; ?>"
+               href="<?php echo esc_url( route_url('groups') ); ?>">
+        <?php echo esc_html( __( "My ", 'disciple-tools-autolink' ) . groups_label() ); ?>
+    </dt-button>
 </div>
