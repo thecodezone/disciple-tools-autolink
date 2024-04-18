a2dcc13002af29f0051322808a3ca06441409183
Craig Wann <craig@codezone.io>
2024-04-18 11:01:44 -0500

    Adding conditional back to tools menu.

--- resources/views/partials/tools-menu.php
+++ resources/views/partials/tools-menu.php
@@ -13,18 +13,20 @@ $action = $action ?? null;
 ?>
 
 <div class="container">
-    <?php if ( function_exists( 'dt_genmapper_metrics' ) ): ?>
-        <dt-button context="<?php echo $action === 'genmap' ? 'primary' : 'inactive'; ?>"
-                   href="<?php echo esc_url( route_url() ); ?>">
-            <?php echo esc_html( __( 'GenMap', 'disciple-tools-autolink' ) ); ?>
-        </dt-button>
-    <?php endif; ?>
-	<dt-button context="<?php echo $action === 'coaching-tree' ? 'primary' : 'inactive'; ?>"
-	           href="<?php echo esc_url( route_url( 'coaching-tree' ) ) ?>">
-		<?php echo esc_html( __( 'Coaching Tree', 'disciple-tools-autolink' ) ); ?>
-	</dt-button>
-    <dt-button context="<?php echo !$action ? 'primary' : 'inactive'; ?>"
-               href="<?php echo esc_url( route_url('groups') ); ?>">
-        <?php echo esc_html( __( "My ", 'disciple-tools-autolink' ) . groups_label() ); ?>
+  <?php if ($churches['total'] > 0): ?>
+      <?php if ( function_exists( 'dt_genmapper_metrics' ) ): ?>
+          <dt-button context="<?php echo $action === 'genmap' ? 'primary' : 'inactive'; ?>"
+                     href="<?php echo esc_url( route_url() ); ?>">
+              <?php echo esc_html( __( 'GenMap', 'disciple-tools-autolink' ) ); ?>
+          </dt-button>
+      <?php endif; ?>
+    <dt-button context="<?php echo $action === 'coaching-tree' ? 'primary' : 'inactive'; ?>"
+               href="<?php echo esc_url( route_url( 'coaching-tree' ) ) ?>">
+      <?php echo esc_html( __( 'Coaching Tree', 'disciple-tools-autolink' ) ); ?>
     </dt-button>
+  <?php endif; ?>
+  <dt-button context="<?php echo !$action ? 'primary' : 'inactive'; ?>"
+             href="<?php echo esc_url( route_url('groups') ); ?>">
+      <?php echo esc_html( __( "My ", 'disciple-tools-autolink' ) . groups_label() ); ?>
+  </dt-button>
 </div>
