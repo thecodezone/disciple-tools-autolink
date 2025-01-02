1c4e755647f0b6b3fd5e7be1436890f7e9e7a0b2
Craig Wann <craig@codezone.io>
2025-01-02 12:23:02 -0600

    updating setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -13,6 +13,7 @@ KEBAB_CASE="dt-autolink"
 EXCLUDE_DIRS="-path ./vendor -o -path ./vendor-scoped -o -path ./node_modules -o -path ./.git -o -path ./.idea"
 
 PLACEHOLDER="dt_plugins"
+-o -path ./bin/setup.sh
 
 # Copy ../.env.example as .env unless it exists
 if [ -f ".env.example" ] && [ ! -f ".env" ]; then
@@ -20,15 +21,13 @@ if [ -f ".env.example" ] && [ ! -f ".env" ]; then
 fi
 
 # Replace strings in files excluding specified directories
-find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT Autolink/$NAME/g"
-find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt\/plugin/$PACKAGE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT Plugin/$NAME/g"
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT\\Plugin/$NAMESPACE/g"
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT\\\\Plugin/$NAMESPACE_ESCAPED/g"
-find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT_Autolink/$UPPER_CAMEL_CASE/g"
-find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt_plugins/$PLACEHOLDER/g"
-find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt_autolink/$SNAKE_CASE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/DT_Plugin/$UPPER_CAMEL_CASE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt_plugin/$SNAKE_CASE/g"
 find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/$PLACEHOLDER/dt_plugins/g"
-find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt-autolink/$KEBAB_CASE/g"
+find ./ \( $EXCLUDE_DIRS \) -prune -o -type f -print0 | xargs -0 perl -pi -e "s/dt-plugin/$KEBAB_CASE/g"
 
 #mv dt-autolink.php $FILENAME
 #rm .github/local.yml
