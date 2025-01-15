883aa7595b4a505d792ad4dfd83e8c3a605ab231
Muniraj <muni20002raj@gmail.com>
2025-01-15 19:48:22 +0530

    The training videos that were added are not showing.

--- src/Controllers/TrainingController.php
+++ src/Controllers/TrainingController.php
@@ -3,17 +3,23 @@
 namespace DT\Autolink\Controllers;
 
 use DT\Autolink\GuzzleHttp\Psr7\Request;
-use DT\Autolink\Services\Options;
+use DT\Autolink\Repositories\TrainingVideoRepository;
 use function DT\Autolink\container;
+use function DT\Autolink\get_plugin_option;
 use function DT\Autolink\template;
 
 class TrainingController
 {
-	public function show( Request $request )
-	{
-		$options = container()->get( Options::class );
-		$videos = $options->get( 'training_videos' );
-		$videos = json_decode( $videos );
-		return template( 'training', compact( 'videos' ) );
-	}
+    public function show( Request $request )
+    {
+        $options = container()->get( TrainingVideoRepository::class );
+        $default_training_videos = $options->all();
+        $videos = get_plugin_option( 'training_videos' );
+        if ( !$videos ) {
+            $videos = $default_training_videos;
+        }
+        $videos = json_decode( $videos );
+
+        return template( 'training', compact( 'videos' ) );
+    }
 }
