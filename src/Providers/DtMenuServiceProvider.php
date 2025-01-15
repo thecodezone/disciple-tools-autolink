494b1fea6f5c2bcec7e382f4d04c92754323e7c4
Muniraj <muni20002raj@gmail.com>
2025-01-15 19:45:18 +0530

    "Add main DT menu link" option is not working

--- src/Providers/DtMenuServiceProvider.php
+++ src/Providers/DtMenuServiceProvider.php
@@ -2,11 +2,24 @@
 
 namespace DT\Autolink\Providers;
 
+use DT\Autolink\League\Container\ServiceProvider\BootableServiceProviderInterface;
+use DT\Autolink\League\Container\ServiceProvider\AbstractServiceProvider;
 use function DT\Autolink\get_plugin_option;
 
 
-class DtMenuServiceProvider extends ServiceProvider
-{
+
+class DtMenuServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface{
+    /**
+     * Provide the services that this provider is responsible for.
+     *
+     * @param string $id The ID to check.
+     * @return bool Returns true if the given ID is provided, false otherwise.
+     */
+    public function provides( string $id ): bool
+    {
+        return false;
+    }
+
     /**
      * Register any application services.
      *
@@ -14,19 +27,7 @@ class DtMenuServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-
         // Register the necessary bindings or singletons
-        if ( get_plugin_option( 'show_in_menu' ) ) {
-
-            add_filter('desktop_navbar_menu_options', function ( $menu_options ) {
-                $menu_options[] = [
-                    'label' => __( 'Autolink', 'disciple-tools-autolink' ),
-                    'link' => site_url( '/autolink' ),
-                ];
-
-                return $menu_options;
-            }, 999, 1);
-        }
     }
 
     /**
@@ -36,7 +37,18 @@ class DtMenuServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        static $filter_added = false;
 
-        // Check if the menu should be displayed and add it if necessary
+        if ( !$filter_added && get_plugin_option( 'show_in_menu' ) ) {
+            add_filter('desktop_navbar_menu_options', function ( $menu_options ) {
+                $menu_options[] = [
+                    'label' => __( 'Autolink', 'disciple-tools-autolink' ),
+                    'link' => site_url( '/autolink' ),
+                ];
+
+                return $menu_options;
+            }, 999, 1);
+            $filter_added = true;
+        }
     }
 }
