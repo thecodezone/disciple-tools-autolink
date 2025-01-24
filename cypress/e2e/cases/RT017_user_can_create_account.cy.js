3ac9590aa23b138e1786a14991d2febad5456064
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2025-01-24 11:19:53 +0530

    Cypress Test Cases [RT017]

--- cypress/e2e/cases/RT017_user_can_create_account.cy.js
+++ cypress/e2e/cases/RT017_user_can_create_account.cy.js
@@ -1,19 +1,52 @@
-describe('RT017_user_can_create_account', () => {
+describe("RT017_user_can_create_account", () => {
+  let shared_data = {
+    username: `test`,
+    email: "test@gmail.com",
+    password: "password",
+  };
 
-  let shared_data = {};
+  // before(() => {
+  //   cy.npmAutoLinkInit();
+  // });
 
-  before(() => {
-    cy.npmAutoLinkInit();
-  })
+  // create user account in autolink plugin
+  it("Create user account in autolink plugin", () => {
+    cy.session("dt_frontend_login_and_obtain_autolink_plugin_ml", () => {
+      cy.visit("/autolink/login");
 
-  // XYZ.....
-  it('XYZ.....', () => {
-    cy.session(
-      'xyz_',
-      () => {
-        // TODO....
-      }
-    );
-  })
+      cy.get('dt-button[context="link"]').contains("Create Account").click();
 
-})
+      cy.get('dt-text[name="username"]')
+        .shadow()
+        .find("input")
+        .type(shared_data.username);
+
+      //email
+      cy.get('dt-text[name="email"]')
+        .shadow()
+        .find("input")
+        .type(shared_data.email);
+      //password
+      cy.get('dt-text[name="password"]')
+        .shadow()
+        .find("input")
+        .type(shared_data.password);
+
+      //conform password
+      cy.get('dt-text[name="confirm_password"]')
+        .shadow()
+        .find("input")
+        .type(shared_data.password);
+
+      cy.get('dt-button[context="success"]').shadow().find("button").click();
+
+      cy.get("form").submit();
+
+      cy.get("al-menu").click();
+
+      cy.get("al-menu").shadow().find('a[title="Log Out"]').click();
+    });
+  });
+
+  // login
+});
