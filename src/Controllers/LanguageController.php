7ef0bcde3458e4cecbffff5022bcb70d6fa32069
Muniraj <muni20002raj@gmail.com>
2025-02-11 09:38:32 +0530

    Add Change Language feature to the hamburger menu

---
+++ src/Controllers/LanguageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DT\Autolink\Controllers;
+
+use DT\Autolink\GuzzleHttp\Psr7\Request;
+use DT\Autolink\Services\Language;
+use function DT\Autolink\container;
+use function DT\Autolink\extract_request_input;
+use function DT\Autolink\response;
+
+class LanguageController
+{
+    public function switch( Request $request ){
+
+        $body = extract_request_input( $request );
+        $user_id = get_current_user_id();
+        $result = container()->get( Language::class )->switchUserLocale( $user_id, $body );
+		if ( ! $result ) {
+          return response( [ "message" => "Invalid request" ], 400 );
+        }
+        return response( [ 'success' => true ] );
+    }
+}
