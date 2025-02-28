c02614e01644b20cd5a0361b517370574bdfefee
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2025-02-28 19:35:07 +0530

    Count fields allow scientific notation

--- resources/js/dom-hooks/_church-counts.js
+++ resources/js/dom-hooks/_church-counts.js
@@ -1,16 +1,40 @@
 export default (el) => {
+  const validateInputField = (inputField) => {
+    if (inputField) {
+      inputField.addEventListener("keypress", function (evt) {
+        if (
+          (evt.which != 8 && evt.which != 0 && evt.which < 48) ||
+          evt.which > 57
+        ) {
+          alert("Please enter a valid number");
+          evt.preventDefault();
+        }
+      });
+
+      inputField.addEventListener("input", function (evt) {
+        const value = evt.target.value;
+
+        if (/e/i.test(value)) {
+          evt.target.value = value.replace(/e/gi, "");
+        }
+      });
+    }
+  };
+
   el.addEventListener("change", (event) => {
     let postID = event.srcElement.postID;
+
     if (!event.detail) {
       return;
     }
+
     let updatedValue = event.detail.newValue;
     let counter = document.querySelector(
       '[data-churchid="' +
-      postID +
-      '"][data-field="' +
-      event.srcElement.name +
-      '"]'
+        postID +
+        '"][data-field="' +
+        event.srcElement.name +
+        '"]',
     );
 
     if (!counter) {
@@ -22,6 +46,11 @@ export default (el) => {
     if (!numberBadge) {
       return;
     }
+
     numberBadge.innerHTML = updatedValue;
+
+    // Validate the input field
+    const inputField = event.srcElement;
+    validateInputField(inputField);
   });
-}
+};
