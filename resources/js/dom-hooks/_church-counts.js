ed6b9b3c1f4687ca76a379eb98af3f42063b84b3
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2025-02-28 19:47:35 +0530

    Update _church-counts.js

--- resources/js/dom-hooks/_church-counts.js
+++ resources/js/dom-hooks/_church-counts.js
@@ -6,7 +6,6 @@ export default (el) => {
           (evt.which != 8 && evt.which != 0 && evt.which < 48) ||
           evt.which > 57
         ) {
-          alert("Please enter a valid number");
           evt.preventDefault();
         }
       });
